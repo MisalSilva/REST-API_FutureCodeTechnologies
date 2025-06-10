efc41f8271ca38e353e8aa359ee9a83a1bba1502
MisalSilva <misal.20230183@iit.ac.lk>
2025-06-10 21:25:11 +0530

    Add index.js to initialize Sequelize and import User and Product models

---
+++ models/index.js
@@ -0,0 +1,31 @@
+// models/index.js
+const { Sequelize, DataTypes } = require('sequelize');
+const dotenv = require('dotenv');
+dotenv.config();
+
+// Create Sequelize instance
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+  }
+);
+
+// Test DB connection
+sequelize.authenticate()
+  .then(() => console.log('✅ Database connected...'))
+  .catch(err => console.error('❌ Error connecting to DB:', err));
+
+// Initialize models
+const db = {};
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+
+// Import models
+db.User = require('./User')(sequelize, DataTypes);
+db.Product = require('./Product')(sequelize, DataTypes);
+
+module.exports = db;

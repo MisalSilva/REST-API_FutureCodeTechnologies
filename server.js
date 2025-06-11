595e8ecc312f97f64289d6963e2d04f6e6ff6452
MisalSilva <misal.20230183@iit.ac.lk>
2025-06-12 02:04:29 +0530

    Add server.js to set up Express server and define API routes

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const db = require('./models');
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+// Routes
+const authRoutes = require('./routes/authRoutes');
+const productRoutes = require('./routes/productRoutes');
+
+app.use('/api/auth', authRoutes);
+app.use('/api/products', productRoutes);
+
+// Sync DB and start server
+db.sequelize.sync().then(() => {
+  app.listen(process.env.PORT, () => {
+    console.log(`Server running on port ${process.env.PORT}`);
+  });
+});

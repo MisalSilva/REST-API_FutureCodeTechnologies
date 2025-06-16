89bf5c5b40699d37c4477daf6d8ad005db2d6e73
MisalSilva <misal.20230183@iit.ac.lk>
2025-06-16 19:48:32 +0530

    Remove unnecessary comment line in index.js for cleaner code

--- models/index.js
+++ models/index.js
@@ -1,5 +1,4 @@
-// models/index.js
-const { Sequelize, DataTypes } = require('sequelize');
+const { Sequelize, DataTypes } = require('sequelize'); 
 const dotenv = require('dotenv');
 dotenv.config();
 
